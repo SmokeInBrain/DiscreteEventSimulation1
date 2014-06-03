41ab1ed557f41336872560b63c60467ea8eaddd1
dwladdimiroc <daniel.wladdimiro@gmail.com>
2014-06-03 01:23:21 -0400

    Skeleton code compiled 1.7 fix

--- Code/planningprocessor/src/main.cpp
+++ Code/planningprocessor/src/main.cpp
@@ -9,21 +9,19 @@ int main(int argc, char * argv[])
     File file = File();
     file.getOptions(argc, argv);
     file.readInput();
-<<<<<<< HEAD
     
     Processing processing = Processing(file.getStdIn());
-=======
 
     cout << "Numero de procesos: " << file.getStdIn().getNumProc();
 
     /*
     Processing processing();
->>>>>>> c3e609cd5418e2958a0d345181af2507f6959d7f
     if(processing.planificationProcess()){
         cout<<"True";
     }else{
         cout<<"False";
     }
+    */
     
 
     return 0;
